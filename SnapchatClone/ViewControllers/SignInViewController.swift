a4c2f81c562b58cee2e37ad17d606dbe59403c4c
Erdem Siyam <erdemsiyam@gmail.com>
2022-04-06 23:44:30 +0300

    ui hazırlanması

---
+++ SnapchatClone/ViewControllers/SignInViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  SnapchatClone
+//
+//  Created by Erdem Siyam on 6.04.2022.
+//
+
+import UIKit
+
+class SignInViewController: UIViewController {
+    
+    @IBOutlet weak var txtEmail: UITextField!
+    @IBOutlet weak var txtUsername: UITextField!
+    @IBOutlet weak var txtPassword: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func btnSignInOnClick(_ sender: Any) {
+    }
+    @IBAction func btnSignUpOnClick(_ sender: Any) {
+    }
+}
+
